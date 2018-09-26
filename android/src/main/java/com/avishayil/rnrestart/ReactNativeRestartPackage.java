b311f38956fc6b54687d8d4211b3da7217a9817f
Samvel Avanesov <seavan@gmail.com>
2018-09-25 22:11:52 -0400

    chore: support RN 0.55

--- android/src/main/java/com/avishayil/rnrestart/ReactNativeRestartPackage.java
+++ android/src/main/java/com/avishayil/rnrestart/ReactNativeRestartPackage.java
@@ -23,7 +23,7 @@ public class ReactNativeRestartPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
